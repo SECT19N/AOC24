932c00453ebc3ff595b69958b8aaf71e1477dc2e
alex mason <alexmas9n@gmail.com>
2024-12-01 23:00:38 +0300

    Add project files.

---
+++ AOC24/Model/DayOne.cs
@@ -0,0 +1,34 @@
+﻿namespace AOC24.Model;
+
+public class DayOne {
+	public static List<string> ReadString() {
+		return File.ReadAllLines("./Input/DayOne.txt").ToList();
+	}
+
+	public static int Sum() {
+		int sum = 0;
+
+		List<int> leftNums = new();
+		List<int> rightNums = new();
+
+		foreach (string line in ReadString()) {
+			string[] parts = line.Split("   ");
+
+			leftNums.Add(int.Parse(parts[0]));
+			rightNums.Add(int.Parse(parts[1]));
+		}
+
+		leftNums.Sort();
+		rightNums.Sort();
+
+		for (int i = 0; i < leftNums.Count; i++) {
+			sum += Difference(leftNums[i], rightNums[i]);
+		}
+
+		return sum;
+	}
+
+	public static int Difference(int left, int right) {
+		return Math.Abs(left - right);
+	}
+}
---
+++ AOC24/Program.cs
@@ -0,0 +1,9 @@
+﻿using AOC24.Model;
+
+namespace AOC24;
+
+public class Program {
+	static void Main(string[] args) {
+		Console.WriteLine(DayOne.Sum());
+	}
+}
