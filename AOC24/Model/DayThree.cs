59c6de4acdfbf2533204b307db172789705a83ca
alex mason <alexmas9n@gmail.com>
2024-12-03 18:12:30 +0300

    daythree two stars

--- AOC24/Model/DayThree.cs
+++ AOC24/Model/DayThree.cs
@@ -42,4 +42,36 @@ public class DayThree {
 
 		return add;
 	}
+
+	public static int AddUpWithCheck() {
+		int total = 0;
+		bool isMulEnabled = true;
+
+		string instructionPattern = @"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))";
+		MatchCollection instructions = Regex.Matches(ReadString(), instructionPattern);
+
+		foreach (Match instruction in instructions) {
+			string value = instruction.Value;
+
+			if (value.StartsWith("mul")) {
+				if (isMulEnabled) {
+					string regex = @"\d{1,3}";
+					MatchCollection numbers = Regex.Matches(value, regex);
+					int score = 1;
+
+					foreach (Match number in numbers) {
+						score *= int.Parse(number.Value);
+					}
+
+					total += score;
+				}
+			} else if (value == "do()") {
+				isMulEnabled = true;
+			} else if (value == "don't()") {
+				isMulEnabled = false;
+			}
+		}
+
+		return total;
+	}
 }
--- AOC24/Program.cs
+++ AOC24/Program.cs
@@ -4,12 +4,16 @@ namespace AOC24;
 
 public class Program {
 	static void Main(string[] args) {
+		Console.WriteLine("Day One");
 		Console.WriteLine($"Sum of Differences: {DayOne.Sum()}");
 		Console.WriteLine($"Score of similarity: {DayOne.Similiarity()}");
 
+		Console.WriteLine("Day Two");
 		Console.WriteLine($"Safe reports: {DayTwo.CheckSafety()}");
 		Console.WriteLine($"Safe reports with dampeners: {DayTwo.CheckSafetyWithDampeners()}");
 
+		Console.WriteLine("Day Three");
 		Console.WriteLine($"Uncorrupted mul scores: {DayThree.AddUp()}");
+		Console.WriteLine($"Uncorrupted mul scores with checks: {DayThree.AddUpWithCheck()}");
 	}
 }
